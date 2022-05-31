b5c93132643b262de49ac054441d4103250990cf
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-31 11:36:40 -0500

    traverse through the file and add corresponded data to each variable

--- train.py
+++ train.py
@@ -4,3 +4,15 @@ with open('intents.json', 'r') as f:
     intents = json.load(f)
 
 print(intents)
+
+# Hold all words in the json data
+all_words = []
+# Hold all the tag in json data
+tags = []
+# Hold the pattern and tag
+xy = []
+
+for intent in intents['intents']:
+    tags.append(intent['tag'])
+    for pattern in intent['patterns']:
+        pass
