a3e2de33a86d81641edc93db90e22956aaa11b51
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-31 11:14:06 -0500

    initialize stemmer

--- nltk_utils.py
+++ nltk_utils.py
@@ -2,11 +2,13 @@ import nltk
 # nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 
+stemmer = PorterStemmer()
+
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
 def stem(word):
-    pass
+    return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
     pass
