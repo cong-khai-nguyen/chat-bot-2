b8e3fc69871c38e5c2c3fb55d9c8642e70fa7a7f
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-06-03 13:11:36 -0500

    define helper function bag_of_word

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,7 @@
 import nltk
+from train import ignore_words
 # nltk.download('punkt')
+import numpy as np
 from nltk.stem.porter import PorterStemmer
 
 stemmer = PorterStemmer()
@@ -11,4 +13,8 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
+    # Stemmed already tokenized array
+    tokenized_sentence = [stem(w) for w in tokenized_sentence if w not in ignore_words]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+
     pass
