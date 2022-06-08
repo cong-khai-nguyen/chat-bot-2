9d5dde52e24ee0d7ee2b6aadba26602017d646ae
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-06-07 23:19:01 -0500

    import pyTorch

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataSet, DataLoader
 import pandas as pd
 with open('intents.json', 'r') as f:
     intents = json.load(f)
